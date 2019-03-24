4941753e6639a53424c39a482f13b28e9083152a
Rin <radysurya.agus@gmail.com>
2019-03-25 02:53:06 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import * as serviceWorker from './serviceWorker'
 
 const animeScript = document.createElement('script')
 animeScript.src = "https://unpkg.com/animejs@2.2.0/anime.min.js"
-document.body.appendChild(animeScript)
+document.body.append(animeScript)
 
 render(<App />, document.getElementById('root'))
 serviceWorker.unregister()
