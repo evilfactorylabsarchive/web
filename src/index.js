e092ddbbae5bef49e20a19d10f8ebed7a3fbd55a
Fariz Rizaldy <farizrizaldy@outlook.co.id>
2019-04-21 03:52:35 +0700

    refactor: delete old files

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { render } from 'react-snapshot'
-import App from './App'
-import * as serviceWorker from './serviceWorker'
-
-render(<App />, document.getElementById('root'))
-
-serviceWorker.unregister()
