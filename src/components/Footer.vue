42a5179bb6c2ae2f1d2aa6ee62e450df06a2369c
Fariz Rizaldy <farizrizaldy@outlook.co.id>
2019-04-22 11:26:58 +0700

    feat: create Footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <footer class="c-footer cf white f6 mt5">
+    <div class="fl w-30-l w-50-m w-100 tl-ns tc">
+      <p>&copy; {{ new Date().getFullYear() }} • evilfactory</p>
+    </div>
+    <div class="fl w-70-l w-50-m w-100 tr-ns tc">
+      <p>hello@evilfactory.id</p>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+.c-footer {
+  border-top: 1px solid #cccccc;
+  background-color: #000000;
+  padding: 0.5rem 2rem;
+}
+</style>
+
