c3e4bcc347df1feb078d25a228f8b08bcaa6f66e
Fariz Rizaldy <farizrizaldy@outlook.co.id>
2019-04-22 10:32:51 +0700

    feat: integrate with netlify form

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,12 +1,100 @@
 <template>
   <Layout>
     <h1>Contact</h1>
-    <p>Launching a new web application? Need help with an existing project? Send us an email to:</p>
-    <a
-      class="black dim"
-      href="mailto:hello@evilfactory.id"
-    >
-      hello@evilfactory.id
-    </a>
+    <p>Launching a new web application? Need help with an existing project? Let's talk.</p>
+    <form name="contact" method="POST" netlify-honeypot="bot-field" data-netlify="true">
+      <div class="mt3">
+        <label
+          class="f6"
+          for="name"
+        >
+          Name (required)
+        </label>
+        <input
+          class="db ph2 pv1 lh-copy f6 mt1 w-100"
+          id="name"
+          type="text"
+          placeholder="Your name"
+          v-model="name"
+          required
+        >
+      </div>
+      <div class="mt3">
+        <label
+          class="f6"
+          for="email"
+        >
+          Email (required)
+        </label>
+        <input
+          class="db ph2 pv1 lh-copy f6 mt1 w-100"
+          id="email"
+          type="email"
+          placeholder="Your email address so we can get in touch"
+          v-model="email"
+          required
+        >
+      </div>
+      <div class="mt3">
+        <label
+          class="f6"
+          for="website"
+        >
+          Website
+        </label>
+        <input
+          class="db ph2 pv1 lh-copy f6 mt1 w-100"
+          id="website"
+          type="text"
+          placeholder="Your company website URL"
+        >
+      </div>
+      <div class="mt3">
+        <label
+          class="f6"
+          for="notes"
+        >
+          About your project (required)
+        </label>
+        <textarea
+          rows="8"
+          class="db ph2 pv1 lh-copy f6 mt1 w-100"
+          id="notes"
+          v-model="notes"
+          placeholder="How can we help?"
+          required
+        >
+        </textarea>
+      </div>
+      <div class="mv3">
+        <button
+          :disabled="shouldButtonDisabled"
+          type="submit"
+          class="c-button w-100 w-20-ns"
+        >
+          Submit
+        </button>
+      </div>
+    </form>
   </Layout>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    name: '',
+    email: '',
+    notes: ''
+  }),
+  computed: {
+    shouldButtonDisabled () {
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+
+      if (!emailRegex.test(this.email)) return true
+      if (this.name.length < 3 || this.email.length < 3 || this.notes.length < 3) return true
+
+      return false
+    }
+  }
+}
+</script>
