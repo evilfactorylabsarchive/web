912361716e00bb6120a5bba2b5131f6459728826
Fariz Rizaldy <farizrizaldy@outlook.co.id>
2019-04-22 13:48:36 +0700

    chore: cleaning up dependencies

--- gridsome.config.js
+++ gridsome.config.js
@@ -12,56 +12,9 @@ module.exports = {
   },
   plugins: [
     {
-      use: '@gridsome/source-filesystem',
-      options: {
-        path: 'blog/**/*.md',
-        typeName: 'Post',
-        route: '/blog/:year/:slug',
-        refs: {
-          category: {
-            typeName: 'Category',
-            route: '/c/:slug',
-            create: true
-          }
-        }
-      }
-    },
-    {
       use: '@gridsome/plugin-google-analytics',
       options: {
         id: 'UA-126921948-1'
-      }
-    },
-    {
-      use: 'gridsome-plugin-rss',
-      options: {
-        contentTypeName: 'Post',
-        feedOptions: {
-          title: 'Thoughts by evilfactory',
-          feed_url: 'https://evilfactory.id/blog/rss.xml',
-          site_url: 'https://evilfactory.id'
-        },
-        feedItemOptions: node => ({
-          title: node.title,
-          description: node.description,
-          url: 'https://evilfactory.id/blog/' + node.slug,
-          author: 'evilfactory'
-        }),
-        output: {
-          dir: './static/blog/',
-          name: 'rss.xml'
-        }
-      }
-    },
-    {
-      use: '@gridsome/plugin-sitemap',
-      options: {
-        config: {
-          '/blog/*': {
-            changefreq: 'daily',
-            priority: 0.3
-          }
-        }
       }
     }
   ]
