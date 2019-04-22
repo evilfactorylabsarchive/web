a901933d58da7984113c977b92912b18c2b65c29
Fariz Rizaldy <farizrizaldy@outlook.co.id>
2019-04-22 23:16:44 +0700

    perf: load css from local instead from CDN

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import DefaultLayout from '~/layouts/Default.vue'
+import '~/assets/css/tachyons.min.css'
+import '~/assets/css/prism-tomorrow.min.css'
 import '~/assets/css/global.css'
 
 export default function (Vue, { head }) {
@@ -7,15 +9,5 @@ export default function (Vue, { head }) {
   head.link.push({
     rel: 'preconnect',
     href: 'https://www.google-analytics.com'
-  })
-
-  head.link.push({
-    href: 'https://unpkg.com/tachyons@4.10.0/css/tachyons.min.css',
-    rel: 'pefetch stylesheet'
-  })
-  
-  head.link.push({
-    href: 'https://cdnjs.cloudflare.com/ajax/libs/prism/1.15.0/themes/prism-tomorrow.min.css',
-    rel: 'prefetch stylesheet'
   })
 }
